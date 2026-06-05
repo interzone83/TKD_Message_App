e715a4f5f73f16687996526704bb9f5a5d983042
interzone83 <charlie.perera@gmail.com>
2026-06-05 22:40:07 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,1 @@
-const API_URL = "https://script.google.com/macros/s/AKfycbyCQPeVckF0zr3Sk8fV7r-AUKWXeu2hO8ipLn1ky4g_CXCV78HfMuZpumPhTZQ9R-A/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbxtIo7c70HTLxTajALH35SXJxTmowzZ7i3P6zBeVbUxDZo0ts_ZEJjRMIuL5dOEaTHZ9w/exec";
